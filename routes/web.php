b3fe5d636f1b8d567ea0e7dc9506463a6e9a7c00
Balakarthikeyan <balakarthikeyan07@gmail.com>
2024-08-01 13:03:36 +0530

    Added CRUD Example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('notes', [NoteController::class, 'index']);
+// Route::get('notes/create', [NoteController::class, 'create']);
+// Route::post('notes', [NoteController::class, 'store']);
+// Route::get('notes/{id}', [NoteController::class, 'show']);
+// Route::get('notes/{id}/edit', [NoteController::class, 'edit']);
+// Route::put('notes/{id}', [NoteController::class, 'update']);
+// Route::delete('notes/{id}', [NoteController::class, 'destroy']);
+
+Route::resource('notes', NoteController::class);
