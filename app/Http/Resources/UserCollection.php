83e9caeefb4af109b65a66456f4668ef422ff228
Balakarthikeyan <balakarthikeyan07@gmail.com>
2024-08-03 11:43:34 +0530

    Added JsonResource & JsonCollection

---
+++ app/Http/Resources/UserCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "data" => UserResource::collection($this->collection)->additional($this->additional),
+            "status" => 200
+        ];
+
+        // return parent::toArray($request);
+    }
+}
