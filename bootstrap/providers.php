2f7299b4d1dfc34a919ddc64fa15fbdde6592574
Balakarthikeyan <balakarthikeyan07@gmail.com>
2024-08-08 00:29:49 +0530

    Added Telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,6 @@ return [
     App\Providers\CustomFacadeServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Laravel\Passport\PassportServiceProvider::class,
 ];
